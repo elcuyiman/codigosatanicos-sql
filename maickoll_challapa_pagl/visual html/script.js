864b5c4a4429537920561e96f376fc4bd994e626
elcuyiman <maickoll.challapa1@gmail.com>
2026-04-05 20:41:00 -0400

    Add files via upload

---
+++ maickoll_challapa_pagl/visual html/script.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-llamar').addEventListener('click', function() {
+    document.getElementById('mensaje-llamar').textContent = 'optimus prime esta salvando cybertron no puede responder ahora';
+});
+document.getElementById('btn-enviar').addEventListener('click', function() {
+    document.getElementById('mensaje-enviar').textContent = 'gracias por enviar tu datos ahora salvemos cybertron';
+});
